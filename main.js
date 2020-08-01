7d45a89c9cb293c66eef555ad8d848c7ce3b6033
Alok-Senapati <aloksenapati470@gmail.com>
2020-08-01 19:26:23 +0530

    First Commit

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+let res = 0;
+let num = 0;
+let dotFlag = false;
+let signFlag = false;
+let operator = "";
+let displayString = "0";
+
 function initializeScreen() {
+    document.getElementById('display').innerText = displayString;
     for (let i = 1; i < 100; i++) {
         setTimeout(() => {
             document.getElementById('overlay').style.top = i + "vh";
